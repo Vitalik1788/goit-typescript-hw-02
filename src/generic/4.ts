4e35691c383864219370ca3d1bad68577f2e57dd
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-11-15 20:44:54 +0200

    generic task done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface Book {
+  title: any;
+}
+
+class Component<T> {
+  constructor (public props: T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<Book> {
+  pageInfo(): void {
     console.log(this.props.title);
   }
 }
